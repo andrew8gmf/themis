7c3735dfbf6845146366ad1f6b15e21fbf6c656e
andrew8gmf <andrew8gmf@gmail.com>
2020-10-17 20:29:45 -0300

    fix dotenv

--- themis/__main__.py
+++ themis/__main__.py
@@ -1,6 +1,27 @@
 import sys
+import os
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+API_KEY = os.environ.get("API_KEY")
+API_SECRET = os.environ.get("API_SECRET")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
 
 from pyfiglet import Figlet
+
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+print(user)
 
 def main():
     f = Figlet(font='slant')
